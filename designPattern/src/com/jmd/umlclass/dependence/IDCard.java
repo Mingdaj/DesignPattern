9cf46ef52ee608142f65fc69aa1b47d16f0b5239
JiangMingda <15519688306@163.com>
2024-03-12 18:23:02 +0800

    UML类图初探

---
+++ designPattern/src/com/jmd/umlclass/dependence/IDCard.java
@@ -0,0 +1,11 @@
+package com.jmd.umlclass.dependence;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mingda
+ * Time: 2024/3/12 17:28
+ * File: IDCard
+ * Description:
+ */
+public class IDCard {
+}
